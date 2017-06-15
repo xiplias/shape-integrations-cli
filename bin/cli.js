50bd65b7a0f1891ef2885eb230493cc8e12679fa
Anders Hansen <xiplias@gmail.com>
2017-06-15 13:04:21 +0200

    Adding support for create the structue for a new project

--- bin/cli.js
+++ bin/cli.js
@@ -6,14 +6,98 @@ const path = require('path')
 const Table = require('easy-table')
 const chalk = require('chalk')
 const prettyjson = require('prettyjson')
+const fs = require('fs')
 
-program.version('0.0.1').option('-p, --path <path>', 'path for projects')
+program
+  .version('0.0.1')
+  .option('-p, --path <path>', 'path for projects (defaults to projects)')
+
+program
+  .command('create [projectname]')
+  .description('create the structure for a new project')
+  .action(function(projectname) {
+    const testFolder = 'projects'
+    const projectFolder = path.resolve(testFolder, projectname)
+    const testsFolder = path.resolve(projectFolder, 'tests')
+
+    // create projects folder
+    fs.existsSync(testFolder) || fs.mkdirSync(testFolder)
+
+    if (fs.existsSync(projectFolder))
+      return console.log('project already exists')
+
+    // create projectname folder
+    fs.mkdirSync(projectFolder)
+
+    // create project.json file
+    fs.writeFile(
+      path.resolve(projectFolder, 'project.json'),
+      `{
+  "name": "${projectname}",
+  "accessKey": "${projectname}_pass",
+  "base_url": ""
+}`,
+      function(err) {
+        if (err) {
+          return console.log(err)
+        }
+      }
+    )
+
+    // create test folder
+    fs.mkdirSync(testsFolder)
+
+    // create project.json file
+    fs.writeFile(
+      path.resolve(testsFolder, 'first_test.js'),
+      `'use strict'
+
+const request = require('superagent')
+const Ajv = require('ajv')
+
+module.exports = {
+  name: 'Test a endpoint',
+  description: 'Tests getting the users',
+  testFunction: function(context, callback) {
+    request
+      .get('https://jsonplaceholder.typicode.com/users')
+      .end(function(err, res) {
+        if (err) return callback(err)
+        if (!res.ok) return callback(null, res)
+
+        const validator = new Ajv()
+        const schema = {
+          properties: {
+            statusCode: {
+              type: 'integer',
+              minimum: 200,
+              maximum: 200
+            },
+            body: {
+              type: 'array'
+            }
+          }
+        }
+        validator.validate(schema, res)
+        callback(validator.errors, res.body)
+      })
+  }
+}`,
+      function(err) {
+        if (err) {
+          return console.log(err)
+        }
+
+        console.log('project created at ' + projectFolder)
+      }
+    )
+  })
 
 program
   .command('run [projectIdentifier] [testIdentifier]')
   .description('run setup commands for all envs')
   .action(function(projectIdentifier, testIdentifier, options) {
-    const projectPath = program.path || path.join(__dirname)
+    const projectPath = program.path || 'projects'
 
     if (testIdentifier) {
       core.runTest(projectPath, projectIdentifier, testIdentifier, function(
@@ -45,7 +129,7 @@ program
   .command('list [project]')
   .description('run setup commands for all envs')
   .action(function(projectIdentifier, options) {
-    const projectPath = program.path || path.join(__dirname)
+    const projectPath = program.path || 'projects'
 
     if (!projectIdentifier) {
       listProjects(projectPath)
